c08d4f5f0c68c3289445941a0b1ec349e3e03959
XXLZopes <afordm99@gmail.com>
2020-12-10 17:38:45 -0800

    asks all the questions

--- index.js
+++ index.js
@@ -1,46 +1,66 @@
 
 const inquirer = require('inquirer')
 // array of questions for user
-const promptUser = () => {
-    return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your name? (Required)",
-        validate: (nameInput) => {
-          if (nameInput) {
-            return true;
-          } else {
-            console.log("Please enter your name!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Enter your Github Username (Required)",
-        validate: (githubInput) => {
-          if (githubInput) {
-            return true;
-          } else {
-            console.log("Please enter a Github Username!");
-            return false;
-          }
-        },
-      },
-      {
-        type: "confirm",
-        name: "confirmAbout",
-        message:
-          'Would you like to enter some information about yourself for an "About" section?',
-        default: true,
-      },
-    ]);
-  };
-const questions = [
-
-];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'What is the name of your project?'
+    },
+    {
+      type: 'input',
+      name: 'Description',
+      message: 'Describe the project'
+    },
+    {
+      type: 'confirm',
+      name: 'confirmTableOfContents',
+      message: 'Would you like a table of contents?',
+      default: false
+    },
+    {
+      type: 'input',
+      name: 'tableOfContents',
+      message: 'enter in table of contents',
+      when: ({confirmTableOfContents}) => {
+        if (confirmTableOfContents) {
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What are the steps required to install the project?'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Describe how the project will be used'
+    },
+    {
+      type: 'confirm',
+      name: 'confirmScreenshot',
+      message: 'Would you like to add screen shots?',
+      default: false
+    },
+    {
+      type: 'input',
+      name: 'screenshot',
+      message: 'Enter the screenshot pathway from the root directory',
+      when: ({confirmScreenshot}) => {
+        if (confirmScreenshot) {
+          return true;
+        } else{
+          return false;
+        }
+      }
+    },
+  ]);
+};
 
 // function to write README file
 function writeToFile(fileName, data) {
@@ -54,4 +74,7 @@ function init() {
 // function call to initialize program
 init();
 
-promptUser();
+questions()
+.then(testConsoleLog => {
+  console.log(testConsoleLog)
+});
